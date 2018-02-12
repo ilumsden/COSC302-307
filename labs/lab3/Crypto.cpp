ee33ff10765d2d9f69efe6d7ec0967d14252865c
ilumsden <lumsden.ian@gmail.com>
2018-02-12 12:22:09 -0500

    Adds my current progress to encode.

--- labs/lab3/Crypto.cpp
+++ labs/lab3/Crypto.cpp
@@ -1,13 +1,64 @@
 #include <cstdio>
 #include <cstring>
+#include <iostream>
 
 using namespace std;
 
 #include "support.h"
 
-void encode(...) 
+void set_pixel_list(vector<pixel> &pixlist, ppm &img)
 {
-  write this
+    int nrows = img.get_Nrows();
+    int ncols = img.get_Ncols();
+    for (int i = 0; i < nrows; i+=2)
+    {
+        for (int j = 0; j < ncols; j+=2)
+        {
+            pixlist.pushback(pixel(i, j));
+        }
+    }
+    return;
+}
+
+void encode(ppm &img)
+{
+    vector<pixel> pixlist;
+    set_pixel_list(pixlist, img);
+    int row, col;
+    int i = 0;
+    int color = 0;
+    while (1)
+    {
+        char c = cin.get();
+        if (c == cin.eof())
+        {
+            break;
+        }
+        row = pixlist[i].row;
+        col = pixlist[i].col;
+        RGB *pix = img[row] + col;
+        if (color == 0)
+        {
+            pix->R &= 0xFE;
+            pix->R |= (c & 0x1);
+        }
+        else if (color == 1)
+        {
+            pix->G &= 0xFE;
+            pix->G |= (c & 0x1);
+        }
+        else
+        {
+            pix->B &= 0xFE;
+            pix->B |= (c & 0x1);
+        }
+        i++;
+        color++;
+        if (color == 3)
+        {
+            color = 0;
+        }
+    }
 }
 
 void decode(...) 
@@ -39,11 +90,11 @@ int main(int argc, char *argv[])
 
     if (strcmp(argv[1], "-encode") == 0)
     {
-        encode(fname, img);
+        encode(img);
     }
     else
     {
-        decode(fname, img);
+        decode(img);
     }
 
     img.write(fname);
