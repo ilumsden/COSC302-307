1ad7623956ad499f95ad94909a53dd28bf293107
ilumsden <lumsden.ian@gmail.com>
2018-02-12 11:36:06 -0500

    Completes the main function of Crypto for part 1.

--- labs/lab3/Crypto.cpp
+++ labs/lab3/Crypto.cpp
@@ -1,25 +1,51 @@
-#include <...>
+#include <cstdio>
+#include <cstring>
+
 using namespace std;
 
 #include "support.h"
 
-void encode(...) {
+void encode(...) 
+{
   write this
 }
 
-void decode(...) {
+void decode(...) 
+{
   write this
 }
 
-int main(int argc, char *argv[]) {
-  parse command line arguments
-  if something not right,
-    print error message and exit
+int main(int argc, char *argv[]) 
+{
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage: ./Crypto -encode|decode image.ppm\n");
+        return -5;
+    }
+    if (strcmp(argv[1], "-encode") != 0 && strcmp(argv[1], "-decode") != 0)
+    {
+        fprintf(stderr, "Usage: ./Crypto -encode|decode image.ppm\n");
+        return -6;
+    }
+    string fname = argv[2];
+    if (fname.substr(fname.length() - 4) != ".ppm")
+    {
+        fprintf(stderr, "Usage: ./Crypto -encode|decode image.ppm\n");
+        return -7;
+    }
 
-  ppm img;
-  img.read(fname);
+    ppm img;
+    img.read(fname);
 
-  if (encode) encode(fname, img);
-  else
-  if (decode) decode(fname, img);
+    if (strcmp(argv[1], "-encode") == 0)
+    {
+        encode(fname, img);
+    }
+    else
+    {
+        decode(fname, img);
+    }
+
+    img.write(fname);
+    return 0;
 }
