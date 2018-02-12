519c100051cd3d10e8d1db3a49e59a8534f657b3
ilumsden <lumsden.ian@gmail.com>
2018-02-12 11:20:49 -0500

    Adds my current progress on Lab 3.

---
+++ labs/lab3/support.cpp
@@ -0,0 +1,93 @@
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+#include "support.h"
+
+const char ETX = 0x3;
+
+typedef unsigned char uchar;
+
+void ppm::read(string fname) 
+{ 
+    FILE *fp = fopen(fname.c_str(), "r");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "%s could not be opened.\n", fname.c_str());
+        exit(-1);
+    }
+    char magicnum[2];
+    fscanf(fp, "%s", magicnum);
+    if ("P6" != magicnum)
+    {
+        fprintf(stderr, "%s is not the correct file type.\n", fname.c_str());
+        exit(-2);
+    }
+    int nrows, ncols, maxpixval;
+    fscanf(fp, "%i %i", nrows, ncols);
+    fscanf(fp, "%i", maxpixval);
+    if (maxpixval != 255)
+    {
+        fprintf(stderr, "%s does not have the correct maximum pixel value.\n", fname.c_str());
+        exit(-3);
+    }
+    image.assign(nrows, ncols);
+    int n = 3;
+    uchar buffer[n];
+    int nread_total, nread;
+    int i = 0;
+    int j = 0;
+    RGB **imgdata = image.data();
+    while (1)
+    {
+        nread = fread(buffer, 1, n, fp);
+        nread_total += nread;
+        if (nread_total > 3 * nrows * ncols)
+        {
+            fprintf(stderr, "Number of elements in %s does not match the header data.\n", fname.c_str());
+            exit(-3);
+        }
+        if (nread == 0 && feof(fp))
+        {
+            break;
+        }
+        (*imgdata + j)->R = buffer[0];
+        (*imgdata + j)->G = buffer[1];
+        (*imgdata + j)->B = buffer[2];
+        j++;
+    } 
+    if (nread_total != 3 * nrows * ncols)
+    {
+        fprintf(stderr, "Number of elements in %s does not match the header data.\n", fname.c_str());
+        exit(-3);
+    }
+    return;
+}
+
+void ppm::write(string fname) 
+{ 
+    fname.erase(fname.length()-4);
+    fname = fname + "_wmsg.ppm";
+    FILE *fp = fopen(fname.c_str(), "w+");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "%s could not be opened/created.", fname.c_str());
+        exit(-4);
+    }
+    fprintf(fp, "P6\n");
+    fprintf(fp, "%i %i\n", get_Nrows(), get_Ncols());
+    fprintf(fp, "255\n");
+    RGB **buf = image.data();
+    int nrows = get_Nrows();
+    int ncols = get_Ncols();
+    for (int i = 0; i < nrows * ncols; i++)
+    {
+        fprintf(fp, "%c %c %c  ", (*buf + i)->R, (*buf + i)->G, (*buf + i)->B);
+        if (i != 0 && i % ncols == 0)
+        {
+            fprintf(fp, "\n");
+        }
+    }
+    return;
+}
---
+++ labs/lab3/support.h
@@ -0,0 +1,78 @@
+#ifndef __SUPPORT_H__
+#define __SUPPORT_H__
+
+#include <string>
+
+typedef unsigned char uchar;
+extern const char ETX;
+
+template <typename T>
+class matrix 
+{
+    public:
+        matrix();
+        ~matrix();
+        void assign(int n_Nrows, int n_Ncols);
+        int get_Nrows() const { return Nrows; }
+        int get_Ncols() const { return Ncols; }
+        T* operator[] (int i) { return buf[i]; }
+        T ** data() const { return &buf[0]; }
+    private:
+        int Nrows, Ncols;
+        T **buf;
+};
+
+template <typename T>
+matrix<T>::matrix()
+{
+    Nrows = 0;
+    Ncols = 0;
+    buf = NULL;
+}
+
+template <typename T>
+matrix<T>::~matrix()
+{
+    delete [] buf[0];
+    delete [] buf;
+}
+
+template <typename T>
+void matrix<T>::assign(int n_Nrows, int n_Ncols)
+{
+    Nrows = n_Nrows;
+    Ncols = n_Ncols;
+    buf = new T *[Nrows];
+    buf[0] = new T [Nrows*Ncols];
+    for (int i = 0; i < Nrows; i++)
+    {
+        buf[i] = buf[i-1] + Ncols;
+    }
+}
+
+struct pixel
+{
+    pixel(int n_row=0, int n_col=0) { row = n_row; col = n_col; }
+    int row, col;
+};
+
+struct RGB
+{
+    RGB(char nR=0, char nG=0, char nB=0) { R = nR; G = nG; B = nB; }
+    uchar R, G, B;
+};
+
+class ppm
+{
+    public:
+        ppm() { ; }
+        void read(std::string fname);
+        void write(std::string fname);
+        RGB * operator[] (int i) { return image[i]; }
+        int get_Nrows() { return image.get_Nrows(); }
+        int get_Ncols() { return image.get_Ncols(); }
+    private:
+        matrix<RGB> image;
+};
+
+#endif
