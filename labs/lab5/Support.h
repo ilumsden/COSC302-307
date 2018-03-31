e5c039f1bae3919259acf9faa5414fa430c6e31a
Ian Lumsden <lumsden.ian@gmail.com>
2018-03-31 11:29:06 -0400

    Adds the new functions to Support.h for mazesolve.

--- labs/lab5/Support.h
+++ labs/lab5/Support.h
@@ -2,13 +2,24 @@
 
 class maze
 {
+    struct pathnode
+    {
+        pathnode() { mazeind = 0; parent = 0; }
+        pathnode(int i, int j) { mazeind = i; parent = j; }
+        int mazeind, parent;
+    };
     public:
         maze() { ; }
         ~maze();
         void create_maze(int, int);
         void write_maze();
+        void read_maze();
+        bool solve_maze();
+        bool solve_maze(int, int);
+        void write_path();
     private:
         bool **grid;
+        std::vector<pathnode> path;
         int Nrows, Ncols;
         int size;
 };
